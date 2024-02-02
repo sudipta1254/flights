4c3a0236d2bfb1abeb20a8e09f15d4ff80d22331
Sudipta Singha <singhaashoka03@gmail.com>
2024-02-02 07:57:54 +0530

    Add notification support

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ xt = 1;
 
 function main(updt = 0) {
    if(!txt.val().trim()) {
-      alert('Enter query to continue!');
+      // alert('Enter query to continue!');
+      notif('Enter query to continue!');
       return;
    }
    let url = 'https://airlabs.co/api/v9',
@@ -82,7 +83,8 @@ function realtime(url, updt = 0) {
       });
    })
    .catch(e => {
-      alert(`Realtime error: ${e.message}`);
+      // alert(`Realtime error: ${e.message}`);
+      notif(`Realtime error: ${e.message}`);
    })
    help2()
 }
@@ -277,7 +279,8 @@ function information(url, updt = 0) {
       updateMap(dts.lat, dts.lng);
    })
    .catch(e => {
-      alert(`Information error: ${e.message}`);
+      // alert(`Information error: ${e.message}`);
+      notif(`Information error: ${e.message}`);
    })
 }
 
@@ -354,6 +357,31 @@ async function help3(code) {
       return data[0].name.common;
    } catch (error) {
       throw new Error(error.message);
+   }
+}
+function notif(msg = "Hi there!") {
+   if (!("Notification" in window)) {
+      // Check if the browser supports notifications
+      alert(msg);
+      
+   } else if (Notification.permission === "granted") {
+      // Check whether notification permissions have already been granted;
+      // if so, create a notification
+
+      const notification = new Notification(msg);
+      notification.addEventListener('click', (event) => {
+         event.preventDefault();
+      })
+      
+   } else if (Notification.permission !== "denied") {
+      // We need to ask the user for permission
+      Notification.requestPermission().then((permission) => {
+         // If the user accepts, let's create a notification
+         if (permission === "granted") {
+            const notification = new Notification(msg);
+            
+         }
+      });
    }
 }
 
