5018cd2bfa0cbece0ae4cf356bd10e43dbf95d3d
Sudipta Singha <115441575+sudipta1254@users.noreply.github.com>
2024-02-11 18:08:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -329,14 +329,14 @@ function help2(x = 0) {
 async function help3(code) {
    // fill.text('Loadixng...');
    try {
-      /*if(code == 'UK')
-         return code;*/
+      if(code == 'UK')
+         return code;
       
-      /*const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
+      const response = await fetch(`https://restcountries.com/v3.1/alpha/${code}`);
       const data = await response.json();
       const countryName = data[0].name.common;
-      return countryName;*/
-      return country[code];
+      return countryName;
+      // return country[code];
    } catch (error) {
       throw new Error(error.message);
    }
